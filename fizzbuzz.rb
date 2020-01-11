c02ecb724c0eda198c8287b3b6645fc70a112158
Franchell Polanco <polanco.franchell@gmail.com>
2020-01-11 15:36:11 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,6 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+def fizzbuzz
+end
+
+def fizzbuzz
+  puts fizzbuzz(3)=="Fizz"
+end
