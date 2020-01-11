99e61453d70bd7a9591ec3cc46ac5f097ea0297e
Franchell Polanco <polanco.franchell@gmail.com>
2020-01-11 16:27:53 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,9 +4,9 @@ def fizzbuzz(number)
 
   if number % 3 == 0
     puts "Fizz"
-  elsif number % 5==0
+  elsif number % 5 == 0
     puts "Buzz"
-  elsif number % 3==0 && number % 5 == 0
+  elsif number % 3 == 0 && number % 5 == 0
     puts "FizzBuzz"
   else
    puts "nil"
