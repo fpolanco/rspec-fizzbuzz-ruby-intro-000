d7c9cdb67de7b9f2ce600c9dbfe1ab34fe6d3231
Franchell Polanco <polanco.franchell@gmail.com>
2020-01-11 16:47:08 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,12 @@
 
 def fizzbuzz(number)
 
+  if number % 3 == 0 && number % 5 == 0
+   "FizzBuzz"
   if number % 3 == 0
      "Fizz"
   elsif number % 5 == 0
      "Buzz"
-  elsif number % 3 == 0 && number % 5 == 0
-     "FizzBuzz"
   else
     "nil"
  end
